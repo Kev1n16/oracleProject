70d4904a7e0aec5f3f24aea2729b797de05dc5b4
Kevin <madandmajestic@yahoo.com>
2022-10-31 17:55:28 -0700

    added templates for html but couldnt connect yet

--- oracleProject/main/views.py
+++ oracleProject/main/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>main page!</h1>")
+def main(response):
+    return HttpResponse("<h1>Main page!</h1>")
 
 def login(response):
     return HttpResponse("<h1>Login page!</h1>")
 
 def create(response):
-    return HttpResponse("<h1>Create account page!</h1>")
+    return render(response, "templates/main/base.html", {})
+
+"""Still trying to link this to an actual html file
+def create(response):
+    return render(response, "templates/main/base.html", {})
+    ...doesnt work"""
