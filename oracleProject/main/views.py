e6075ea961a5a5b796edf942a6f7057f521c18e0
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2022-11-08 21:30:31 -0800

    Fixed HTML Paths (Don't ask how just look at the file differences)
    Signed-off-by: Michael Nguyen <26033112+16-Bits@users.noreply.github.com>

--- oracleProject/main/views.py
+++ oracleProject/main/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def main(response):
-    return HttpResponse("<h1>Main page!</h1>")
+def main(request):
+    return render(request, "main/home.html",)
 
-def login(response):
-    return HttpResponse("<h1>Login page!</h1>")
+def login(request):
+    return render(request, "login/login.html",)
 
-def create(response):
-    return render(response, "templates/main/base.html", {})
+def create(request):
+    return render(request, "create/createaccount.html",)
 
 """Still trying to link this to an actual html file
 def create(response):
