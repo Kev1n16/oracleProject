9f7d5d380fcb466b56d81db99a65393a23daccc9
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2023-05-11 21:36:33 -0700

    Enemble of Lasso, Ridge and ElasticNet

--- oracleProject/main/machinelearning.py
+++ oracleProject/main/machinelearning.py
@@ -3,14 +3,22 @@ from urllib.request import urlopen
 import pandas as pd
 import sqlite3
 from numpy import arange
-from sklearn.linear_model import MultiTaskLassoCV
+from sklearn.linear_model import MultiTaskLassoCV, RidgeCV, MultiTaskElasticNetCV
 from sklearn.model_selection import RepeatedKFold
 import zipfile as zpf
 import joblib
+from sklearn import preprocessing
 
-def MTLassoCV(userData):
-    dataKeys = ["CPU cores", "CPU capacity provisioned [MHZ]", "CPU usage [%]", "Memory capacity provisioned [KB]", "Memory usage [KB]"]
-    formKeys = ['amt_CPU', 'amt_vCPU', 'prcnt_CPU', 'amt_Memory', 'prcnt_Memory']
+
+def machineLearning(userData):
+    # attribute keys based on dataset
+    dataKeys = ["CPU cores", "CPU capacity provisioned [MHZ]",
+                "CPU usage [%]", "Memory capacity provisioned [KB]", "Memory usage [KB]"]
+    # attribute keys based on input form
+    formKeys = ['amt_CPU', 'amt_vCPU',
+                'prcnt_CPU', 'amt_Memory', 'used_Memory']
+
+    # download and clean dataset
     url = 'https://github.com/sevaiq/Datacenter_dataset/raw/master/Datacenter%20dataset.zip'
     responce = urlopen(url)
     zp = zpf.ZipFile(BytesIO(responce.read()))
@@ -22,74 +30,95 @@ def MTLassoCV(userData):
         df.drop(df[df['CPU usage [%]'] < 1].index, inplace=True)
         dfList.append(df)
     fullData = pd.concat(dfList, axis=0, ignore_index=True)
-    df = fullData.sample(n=500)
-    print ("Training Data Loaded")
-    data = df[dataKeys]
-    #define cross-validation method to evaluate model
+    df = [fullData.sample(n=500), fullData.sample(n=500),
+          fullData.sample(n=500)]
+    data = [df[0][dataKeys], df[1][dataKeys], df[2][dataKeys]]
+    print("Training Data Loaded")
+    # define cross-validation method to evaluate model
     cv = RepeatedKFold()
-    model = MultiTaskLassoCV(cv=cv, n_jobs=-1)
-    print ("MultiTaskLassoCV Loaded")
+    # load models
+    modelMTLassoCV = MultiTaskLassoCV(cv=cv, n_jobs=-1)
+    print("MultiTaskLassoCV Loaded")
+    modelRidgeCV = RidgeCV(cv=cv)
+    print("RidgeCV Loaded")
+    modelMTElasticNetCV = MultiTaskElasticNetCV(cv=cv, n_jobs=-1)
+    print("MultiTaskElasticNetCV Loaded")
 
+    # convert from input form keys to dataset keys
     for key1, key2 in zip(dataKeys, formKeys):
         userData[key1] = userData.pop(key2)
-    '''
-    userData['CPU cores'] = userData.pop('amt_CPU')
-    userData['CPU capacity provisioned [MHZ]'] = userData.pop('amt_vCPU')
-    userData['CPU usage [%]'] = userData.pop('prcnt_CPU')
-    userData['Memory capacity provisioned [KB]'] = userData.pop('amt_Memory')
-    userData['Memory usage [KB]'] = userData.pop('prcnt_Memory')
-    '''  
+    # find empty attributes from user input as ML target
     non_empty_keys = [key for key, value in userData.items() if value]
     empty_keys = [key for key, value in userData.items() if not value]
     print(non_empty_keys)
-    print (empty_keys)
+    print(empty_keys)
 
-    x = data[non_empty_keys]
-    y = data[empty_keys]
-    model.fit(x.values,y.values)
-    print ("MultiTaskLassoCV Fitted")
-    
+    # fit training input data and target output data to models
+    x = [data[0][non_empty_keys], data[1][non_empty_keys], data[2][non_empty_keys]]
+    y = [data[0][empty_keys], data[1][empty_keys], data[2][empty_keys]]
+    modelMTLassoCV.fit(x[0].values, y[0].values)
+    print("MultiTaskLassoCV Fitted")
+    modelRidgeCV.fit(x[1].values, y[1].values)
+    print("RidgeCV Fitted")
+    modelMTElasticNetCV.fit(x[2].values, y[2].values)
+    print("MultiTaskElasticNetCV Fitted")
+
+    # dictionaries of final data for user
     inputData = {}
-    predictData = {}
+    predictData = [{}, {}, {}]
+    outputData = {}
+    # add user input data to dictionaries
     for key in non_empty_keys:
         inputData[key] = userData[key]
-        predictData[key] = userData[key]
+        predictData[0][key] = userData[key]
+        predictData[1][key] = userData[key]
+        predictData[2][key] = userData[key]
+    print("Input:")
     print(inputData)
-    prediction = model.predict([list(inputData.values())])
+    predictMTLassoCV = modelMTLassoCV.predict([list(inputData.values())])
+    print("MTLassoCV:")
+    print(predictMTLassoCV)
+    predictRidgeCV = modelRidgeCV.predict([list(inputData.values())])
+    print("RidgeCV:")
+    print(predictRidgeCV)
+    predictMTElasticNetCV = modelMTElasticNetCV.predict(
+        [list(inputData.values())])
+    print("MultiTaskElasticNetCV:")
+    print(predictMTElasticNetCV)
+
+    # format predict data into dictionary
     i = 0
     for key in empty_keys:
-        predictData[key] = round(prediction[0][i],2)
-        i+=1
+        '''
+        predictData[0][key] = round(predictMTLassoCV[0][i], 2)
+        predictData[1][key] = round(predictRidgeCV[0][i], 2)
+        predictData[2][key] = round(predictMTElasticNetCV[0][i], 2)
+        '''
+        valMTLassoCV = round(predictMTLassoCV[0][i], 2)
+        valRidgeCV = round(predictRidgeCV[0][i], 2)
+        valMTElasticNetCV = round(predictMTElasticNetCV[0][i], 2)
+        outputData[key] = (valMTLassoCV + valRidgeCV + valMTElasticNetCV) / 3
+        i += 1
+    print("output1:")
+    print(outputData)
+    print("Input:")
+    print(inputData)
+    # include user input data
+    outputData.update(inputData)
+    print("output2:")
+    print(outputData)
+
+    # convert from dataset keys to input form keys
     for key1, key2 in zip(dataKeys, formKeys):
-        predictData[key2] = predictData.pop(key1)
-    print(predictData)
-    '''
-
-    predictData = inputData.update(outputData)
-    
-    formatData = {
-        "CPU cores":round(prediction[0][0]),
-        "CPU capacity provisioned [MHZ]":round(prediction[0][1],2),
-        "CPU capacity provisioned [GHZ]":round(prediction[0][1]/1000,2),
-        "CPU usage [%]":testData[0],
-        "Memory capacity provisioned [KB]":round(prediction[0][2],2),
-        "Memory capacity provisioned [GB]":round(prediction[0][2]/1e+6,2),
-        "Memory usage [KB]":testData[1],
-        "Memory usage [GB]":round(testData[1]/1e+6,2),
-    }
-
-    for item in formatData:
-        print(item + ": " + str(formatData[item]))
-    '''
-    print ("\nDone.")
-    return predictData
-
+        outputData[key2] = outputData.pop(key1)
+    print("output3:")
+    print(outputData)
+    return outputData
 
 
 def main():
-    testLassoCV()
+    machineLearning()
+
 
 if __name__ == "__main__":
     main()
-
-
--- oracleProject/main/views.py
+++ oracleProject/main/views.py
@@ -4,32 +4,36 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import *
 from .models import Flavor
-from main.machinelearning import MTLassoCV
+from main.machinelearning import machineLearning
 
 # Create your views here.
 def main(request):
-    if 'login_status' in request.COOKIES and 'username' in request.COOKIES:  #ensure the username saved from the cookies matches the logged in user's account name
-        context = {  #context must be consistent with all views except create
+    # ensure the username saved from the cookies matches the logged in user's account name
+    if 'login_status' in request.COOKIES and 'username' in request.COOKIES:
+        context = {  # context must be consistent with all views except create
             'userID': request.COOKIES['username'],
             'login_status': request.COOKIES.get('login_status'),
             'acctInfo': Flavor.objects.all(),
         }
-        return render(request, 'main/base.html', context) #since its successful return with context
+        # since its successful return with context
+        return render(request, 'main/base.html', context)
     else:
-        return render(request, 'login/login.html')  #else bring user back to login page
+        # else bring user back to login page
+        return render(request, 'login/login.html')
 
 
 def login(request):
     form = UserInformation()
 
-    if request.method == 'GET': #obtain information from user input
+    if request.method == 'GET':  # obtain information from user input
         form = UserInformation(request.POST)
-        if form.is_valid(): #check if the information fits the form
-            form.save() #if so save it
+        if form.is_valid():  # check if the information fits the form
+            form.save()  # if so save it
         return render(request, 'login/login.html')
 
     if request.method == 'POST':
-        username = request.POST.get('username') #save the user's inputted username
+        # save the user's inputted username
+        username = request.POST.get('username')
         context = {
             'userID': username,
             'login_status': True,
@@ -38,8 +42,10 @@ def login(request):
         response = render(request, 'main/base.html', context)
 
         # cookie settings
-        response.set_cookie('username', username)  #save the username in cookies
-        response.set_cookie('login_status', True)  #set boolean login_status to true
+        # save the username in cookies
+        response.set_cookie('username', username)
+        # set boolean login_status to true
+        response.set_cookie('login_status', True)
 
         return response
 
@@ -48,26 +54,30 @@ def create(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST) #save information
-        if form.is_valid(): #as long as it is valid it will be saved
+        form = UserCreationForm(request.POST)  # save information
+        if form.is_valid():  # as long as it is valid it will be saved
             form.save()
-            user = form.cleaned_data.get('username') #ensure it is a unique username
-            messages.success(request, 'Account was created for ' + user)  #prompt the user that the account was created
+            # ensure it is a unique username
+            user = form.cleaned_data.get('username')
+            # prompt the user that the account was created
+            messages.success(request, 'Account was created for ' + user)
 
-            return redirect('login page')  #user will see the success that it was created at the login page
+            # user will see the success that it was created at the login page
+            return redirect('login page')
 
     context = {'form': form}
     return render(request, "create/register.html", context)
 
+
 def flavor(request):
-    if 'login_status' in request.COOKIES and 'username' in request.COOKIES:  #make sure the user is logged in
+    if 'login_status' in request.COOKIES and 'username' in request.COOKIES:  # make sure the user is logged in
         context = {
             'userID': request.COOKIES['username'],
             'login_status': request.COOKIES.get('login_status'),
             'acctInfo': Flavor.objects.all(),
         }
 
-        if request.method == 'POST': #save all of the user's inputs, these will be the same as the attributes in the flavor form
+        if request.method == 'POST':  # save all of the user's inputs, these will be the same as the attributes in the flavor form
             acctUsername = request.COOKIES['username']
             name = request.POST.get("name")
             id = request.POST.get("id")
@@ -75,10 +85,10 @@ def flavor(request):
             amt_Memory = request.POST.get("amt_Memory")
             amt_Volume = request.POST.get("amt_Volume")
             amt_Ephemeral_Volume = request.POST.get("amt_Ephemeral_Volume")
-            data = Flavor(name=name, id=id, amt_vCPU=amt_vCPU,  #now we save the values into the flavor to store it in the data base
-            amt_Memory=amt_Memory, amt_Volume=amt_Volume, amt_Ephemeral_Volume=amt_Ephemeral_Volume,
-            acctUsername=acctUsername)
-            data.save()  #save it here
+            data = Flavor(name=name, id=id, amt_vCPU=amt_vCPU,  # now we save the values into the flavor to store it in the data base
+                          amt_Memory=amt_Memory, amt_Volume=amt_Volume, amt_Ephemeral_Volume=amt_Ephemeral_Volume,
+                          acctUsername=acctUsername)
+            data.save()  # save it here
             return render(request, 'main/base.html', context)
 
     context = {
@@ -92,11 +102,12 @@ def flavor(request):
 def logout(request):
     response = HttpResponseRedirect(reverse('login page'))
 
-    #deleting cookies when we log out
+    # deleting cookies when we log out
     response.delete_cookie('username')
     response.delete_cookie('login_status')
 
     return response
+
 
 def predict(request):
     form = PredictInputForm()
@@ -104,13 +115,14 @@ def predict(request):
     if request.method == 'POST':
         form = PredictInputForm(request.POST)
         data = request.POST
-        data = {key: data[key] for key in ['amt_CPU', 'amt_vCPU', 'prcnt_CPU', 'amt_Memory', 'prcnt_Memory']}
-        #print(data)
-
+        data = {key: data[key] for key in [
+            'amt_CPU', 'amt_vCPU', 'prcnt_CPU', 'amt_Memory', 'used_Memory']}
+        # print(data)
+        
         if form.is_valid():
             print('Form submitted')
-            context = MTLassoCV(data)
-            #print(dictTest)
+            context = machineLearning(data)
+            # print(dictTest)
             return render(request, "predict/predictResults.html", context)
     context = {
         'form': form
